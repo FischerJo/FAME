ae99e6c933ea066e993f8f8b973eff2f67c9dbb3
Jonas Fischer <fischer@mmci.uni-saarland.de>
2017-03-23 19:31:55 +0100

    Added Buffer size for ref reading

--- CONST.h
+++ CONST.h
@@ -1,9 +1,20 @@
 #ifndef CONST_H
 #define CONST_H
 
-
+// Length of the reads in bp
 constexpr unsigned int READLEN = 100;
 
+// Length of a kmer in bp
 constexpr unsigned int KMERLEN = 20;
+
+// yields value for number of kmers in one read at compile time
+constexpr kPerRead = READLEN - KMERLEN;
+
+// INTERNAL
+
+
+// internal buffer for reading reference file
+constexpr unsigned int BUFSIZE = 256 * 1024;
+
 
 #endif /* CONST_H */
