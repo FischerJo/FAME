0711ab2aed17b4a17ad62d2ce71ab53bd7a878c9
Jonas Fischer <fischer@mmci.uni-saarland.de>
2017-03-23 19:32:52 +0100

    Exported reading to a new reader class

--- RefGenome.cpp
+++ RefGenome.cpp
@@ -3,3 +3,12 @@
 #include "RefGenome.H"
 
 
+using namespace std;
+
+RefGenome::RefGenome(unsigned int cpgs, const vector<const struct CpG>&& cpgTab,
+        const vector<const string>&& genSeq) :
+    ,   cpgNum(cpgs)
+    ,   cpgTable(cpgTab)
+    ,   genomeSeq(genSeq)
+{
+}
--- RefGenome.h
+++ RefGenome.h
@@ -5,37 +5,11 @@
 #include <istream>
 #include <string>
 #include <vector>
-#include <cstdint>
 
 // Project includes
 #include "CONST.h"
+#include "structs.h"
 
-// yields value for number of kmers in one read at compile time
-constexpr kPerRead(unsigned int k, unsigned int rL) {rL - k}
-
-
-struct CpG {
-    // position where this CpG is present in genome
-    // convention: pos points to the location of the C
-    const uint8_t chrom;
-    const unsigned int pos;
-};
-
-
-struct kmer {
-    // pointer to related CpG
-    const struct CpG * cpg;
-    // offset in this CpG, the C of the CpG is offset 0
-    // 2nd highest bit is flag for strand; use STRANDMASK to extract information
-    //      1 -> forward strand
-    //      0 -> reverse strand
-    const uint16_t offset;
-
-};
-
-// Use this mask to extract strand information from kmer.offset
-// offset & STRANDMASK == TRUE   <=>   kmer on forward strand
-constexpr STRANDMASK = 0x4000;
 
 // Class representing the reference genome
 class RefGenome
@@ -45,9 +19,12 @@ class RefGenome
 
         delete RefGenome();
 
-        // read file in FASTA format
-        // Throws out unlocated reads
-        RefGenome(std::ifstream&);
+        // Ctor
+        // Arguments: ( see class members for full information )
+        //      cpgs        number of CpGs in reference genome
+        //      cpgTab      table of all CpGs in reference genome
+        //      genSeq      genomic sequence seperated by chromosome
+        RefGenome(unsigned int cpgs, const std::vector<const struct CpG>&& cpgTab, const vector<const string>&& genSeq);
 
 
         ~RefGenome();
@@ -56,22 +33,24 @@ class RefGenome
         // hash all kmers in all CpGs to _kmerTable using ntHash
         // the kmers are represented in REDUCED alphabet {A,T,G}
         // mapping all Cs to Ts
-        void hashCpgKmers();
+        // generate Bit representation of whole genome for genomeBit
+        // using full alphabet
+        void generateHashes();
 
 
     private:
 
 
         // number of CpGs in reference genome
-        const unsigned int _cpgNum;
+        const unsigned int cpgNum;
 
         // table of all CpGs in reference genome
         // _cpgTable has size _cpgNum
-        const std::vector<const struct CpG> _cpgTable;
+        const std::vector<const struct CpG> cpgTable;
 
         // table of strings holding chromosome code
         // convention: table index 0-21  autosome 1-22, 22-23 allosome X,Y
-        const std::vector<const std::string> _genomeSeq;
+        const std::vector<const std::string> genomeSeq;
 
         // table of bitstrings* holding a bit representation of genomeSeq
         // used as a perfect hash later on
@@ -84,11 +63,11 @@ class RefGenome
         //          N -> 00   DISCARDED for all computations
         //
         // *own implementation of bitstrings
-        const std::vector<const dnaBitStr> _genomeBit;
+        std::vector<const dnaBitStr> genomeBit;
 
         // hash table of all (reduced alphabet) kmers that surround CpGs in reference,
         // hash values are computed using nthash
-        std::vector<const struct kmer> _kmerTable;
+        std::vector<const struct kmer> kmerTable;
 
 
 };
