3466a8b856d532beee92ef6f4091b978422d8a78
Jonas Fischer <fischer@mmci.uni-saarland.de>
2017-03-24 13:29:29 +0100

    Added constants for CpG and chromosome info

--- CONST.h
+++ CONST.h
@@ -1,14 +1,24 @@
 #ifndef CONST_H
 #define CONST_H
 
-// Length of the reads in bp
+// number of chromosomes in organism
+constexpr unsigned int CHROMNUM = 24;
+
+// (more than) size in bp of biggest chromosome in organism
+constexpr unsigned int CHROMMAX = 260000000;
+
+// (more than) number of CpGs in organism
+constexpr unsigned int CPGMAX = 24000000;
+
+
+// maximum read length of the reads in bp
 constexpr unsigned int READLEN = 100;
 
 // Length of a kmer in bp
 constexpr unsigned int KMERLEN = 20;
 
 // yields value for number of kmers in one read at compile time
-constexpr kPerRead = READLEN - KMERLEN;
+// constexpr kPerRead = READLEN - KMERLEN;
 
 // INTERNAL
 
