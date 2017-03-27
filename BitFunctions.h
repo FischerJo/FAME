95fafffa9f3c4a949f18b40ff09830ee79ee9742
Jonas Fischer <fischer@mmci.uni-saarland.de>
2017-03-27 16:36:39 +0200

    Changed implementation of bit reverse to keep DNA encoding intact

--- BitFunctions.h
+++ BitFunctions.h
@@ -9,26 +9,48 @@ namespace BitFun {
 // following TAOCP by Knuth (Vol.4;1A Section 7.1.3 p.13 code from (70)
 
 // constants for bitswap
-constexpr uint64_t mu0 = 0x5555555555555555ULL;
-constexpr uint64_t c4 =  0x0300c0303030c303ULL;
-constexpr uint64_t c8 =  0x00c0300c03f0003fULL;
-constexpr uint64_t c20 = 0x00000ffc00003fffULL;
+// constexpr uint64_t mu0 = 0x5555555555555555ULL;
+// constexpr uint64_t c4 =  0x0300c0303030c303ULL;
+// constexpr uint64_t c8 =  0x00c0300c03f0003fULL;
+// constexpr uint64_t c20 = 0x00000ffc00003fffULL;
 
 // reverses the order of bits in x
+// inline uint64_t rev64(uint64_t x)
+// {
+//
+//     x = ((x >> 1) & mu0) | ((x & mu0) << 1);
+//     uint64_t tmp = (x ^ (x >> 4)) & c4;
+//     x = x ^ tmp ^ (tmp << 4);
+//     tmp = (x ^ (x >> 8)) & c8;
+//     x = x ^ tmp ^ (tmp << 8);
+//     tmp = (x ^ (x >> 20)) & c20;
+//     x = x ^ tmp ^ (tmp << 20);
+//     return ((x >> 34) | (x << 30));
+//
+// }
+
+// METHOD FOR REVERSING bits OF 64bit KEEPING PAIRS OF BITS INTACT (i.e. our encoding)
+// so x_63 x_62 x_61 ... x_3 x_2 x_1 x_0
+// to x_1 x_0 x_3 x_2 ... x_63 x_62
+// following TAOCP by Knuth (Vol.4;1A Section 7.1.3 p.13 code from (50)
+// adapted code to keep pairs intact (i.e. skip first swap)
+//
+
+// constants for bitswap
+constexpr uint64_t mu1 = 0x3333333333333333ULL;
+constexpr uint64_t mu2 = 0x0f0f0f0f0f0f0f0fULL;
+constexpr uint64_t mu3 = 0x00ff00ff00ff00ffULL;
+constexpr uint64_t mu4 = 0x0000ffff0000ffffULL;
+
 inline uint64_t rev64(uint64_t x)
 {
-
-    x = ((x >> 1) & mu0) | ((x & mu0) << 1);
-    uint64_t tmp = (x ^ (x >> 4)) & c4;
-    x = x ^ tmp ^ (tmp << 4);
-    tmp = (x ^ (x >> 8)) & c8;
-    x = x ^ tmp ^ (tmp << 8);
-    tmp = (x ^ (x >> 20)) & c20;
-    x = x ^ tmp ^ (tmp << 20);
-    return ((x >> 34) | (x << 30));
+    x = ((x >> 2) & mu1) | ((x & mu1) << 2);
+    x = ((x >> 4) & mu2) | ((x & mu2) << 4);
+    x = ((x >> 8) & mu3) | ((x & mu3) << 8);
+    x = ((x >> 16) & mu4) | ((x & mu4) << 16);
+    return (x >> 32) | (x << 32);
 
 }
-
 
 // compute the reverse complement of DNA bitstring with encoding
 // A -> 00
