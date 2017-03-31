a635666ab0cd03be828b241c1f8ae887f720fbae
Jonas Fischer <fischer@mmci.uni-saarland.de>
2017-03-31 18:14:20 +0200

    Fixed size bug (off by factor of 2 bec. of encoding)

--- DnaBitStr.cpp
+++ DnaBitStr.cpp
@@ -7,16 +7,17 @@ using namespace std;
 
 DnaBitStr::DnaBitStr(const unsigned int s) :
         size(s)
-    ,   bitSeq((size / 64) + 1)
-    ,   bitMask((size / 64) + 1)
-    ,   bitRevMask((size / 64) + 1)
+    ,   bitSeq((size / 32) + 1)
+    ,   bitMask((size / 32) + 1)
+    ,   bitRevMask((size / 32) + 1)
 {
 }
 
 
 
-void DnaBitStr::setBitStrLast(string& seq)
+void DnaBitStr::setBitStrLast(string&& seq)
 {
+
 
     uint64_t bitStr = 0;
     uint64_t bitM = 0xffffffffffffffffULL;
@@ -24,38 +25,35 @@ void DnaBitStr::setBitStrLast(string& seq)
     for (unsigned int i = 1; i <= seq.size(); ++i)
     {
 
+        const unsigned int shift = 64 - 2*i;
         // we don't need A here
         switch (seq[i - 1]){
 
             case 'C':
                 {
-                    const unsigned int shift = (64 - 2*i);
                     bitStr |= (1ULL << shift);
-                    bitM ^= (3ULL << shift);
+                    bitM ^= (2ULL << shift);
                 }
                 break;
 
             case 'c':
                 {
-                    const unsigned int shift = (64 - 2*i);
                     bitStr |= (1ULL << shift);
-                    bitM ^= (3ULL << shift);
+                    bitM ^= (2ULL << shift);
                 }
                 break;
 
             case 'G':
                 {
-                    const unsigned int shift = (64 - 2*i);
                     bitStr |= (2ULL << shift);
-                    bitRevM ^= (3ULL << shift);
+                    bitRevM ^= (2ULL << shift);
                 }
                 break;
 
             case 'g':
                 {
-                    const unsigned int shift = (64 - 2*i);
                     bitStr |= (2ULL << shift);
-                    bitRevM ^= (3ULL << shift);
+                    bitRevM ^= (2ULL << shift);
                 }
                 break;
 
@@ -67,12 +65,13 @@ void DnaBitStr::setBitStrLast(string& seq)
                 bitStr |= (3ULL << (64 - 2*i));
                 break;
 
+
             // unknowns, A and N will be encoded as zero; nothing to do
             default:
                 break;
         }
     }
-    bitSeq[size-1] = bitStr;
-    bitMask[size-1] = bitM;
-    bitRevMask[size-1] = bitRevM;
+    bitSeq[size/32] = bitStr;
+    bitMask[size/32] = bitM;
+    bitRevMask[size/32] = bitRevM;
 }
