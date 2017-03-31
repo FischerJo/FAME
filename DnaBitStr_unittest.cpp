9d72d968e3ffe071f90a98fc590a3f1855373819
Jonas Fischer <fischer@mmci.uni-saarland.de>
2017-03-31 13:17:06 +0200

    Added simple test for 32bp pure A sequence

--- DnaBitStr_unittest.cpp
+++ DnaBitStr_unittest.cpp
@@ -3,8 +3,40 @@
 #include "DnaBitStr.h"
 
 
-// Test setting a simple 32bp sequence
+// Test setting and reading a simple 32bp sequence of As
 TEST(DnaBitStr_test, setSimple1)
+{
+    std::string seq = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+    uint64_t bitEnc = 0x0000000000000000ULL;
+    uint64_t bitMask = 0xffffffffffffffffULL;
+    uint64_t revBitEnc = 0xffffffffffffffffULL;
+
+    DnaBitStr bitstr(32);
+    bitstr.setBitStrN(seq, 0);
+
+    uint64_t bits, bitsRev, mask, maskRev;
+    // align kmer to lower bits
+    constexpr unsigned int shiftRight = (64 - (2 * MyConst::KMERLEN) );
+    for (unsigned int i = 0; i < (32 - MyConst::KMERLEN + 1); ++i)
+    {
+
+        ASSERT_EQ( (bitEnc << 2*i) >> shiftRight, bits);
+
+        bitsRev = bitstr.getSeqKmerRev(i);
+        ASSERT_EQ( (revBitEnc << 2*i) >> shiftRight, bitsRev);
+
+        mask = bitstr.getMaskKmer(i);
+        ASSERT_EQ( (bitMask << 2*i) >> shiftRight, mask);
+
+        maskRev = bitstr.getMaskKmerRev(i);
+        ASSERT_EQ( (bitMask << 2*i) >> shiftRight, maskRev);
+    }
+
+
+}
+
+// Test setting and reading a 32bp sequence
+TEST(DnaBitStr_test, setComplex1)
 {
     // encoding: 1001 1111 0010 0111 1000 0001 1011 0010 0111 1000 1101 1011 1000 1110 0110 1110
     std::string seq = "GCTTAGCTGAACGTAGCTGATCGTGATGCGTG";
@@ -19,11 +51,11 @@ TEST(DnaBitStr_test, setSimple1)
     bitstr.setBitStrN(seq, 0);
 
     uint64_t bits, bitsRev, mask, maskRev;
+    // align kmer to lower bits
+    constexpr unsigned int shiftRight = (64 - (2 * MyConst::KMERLEN) );
     for (unsigned int i = 0; i < (32 - MyConst::KMERLEN + 1); ++i)
     {
 
-        // align kmer to lower bits
-        constexpr unsigned int shiftRight = (64 - (2 * MyConst::KMERLEN) );
         bits = bitstr.getSeqKmer(i);
         ASSERT_EQ( (bitEnc << 2*i) >> shiftRight, bits);
 
