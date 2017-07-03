70861032e4280935edb63363f4fac6cb4ced56f6
Jonas Fischer <fischer@mmci.uni-saarland.de>
2017-07-03 18:21:27 +0200

    Incorporated read tags for source in reference

--- Synth/main.cpp
+++ Synth/main.cpp
@@ -15,26 +15,29 @@ int main(int argc, char** argv)
 
     SynthDS synthGen(refLen);
 
-    std::vector<std::string> fwdReads = synthGen.genReadsFwdFixed(100, 10000000, 1);
+    std::vector<size_t> offsets;
+    std::vector<std::string> fwdReads = synthGen.genReadsFwdFixed(100, 100000, 1, offsets);
 
-    std::vector<std::string> revReads = synthGen.genReadsRevFixed(100, 10000000, 1);
+    std::vector<std::string> revReads = synthGen.genReadsRevFixed(100, 100000, 1);
 
 
-    std::ofstream ofsRef("genRef.fasta");
+    std::ofstream ofsRef("genRef_1err.fasta");
     ofsRef << ">CHRSYNTH1\n";
     ofsRef << synthGen.getRef();
     ofsRef.close();
 
-    std::ofstream ofsReads("genReads.fastq");
+    std::ofstream ofsReads("genReadsFwd_1err.fastq");
 
     for (size_t i = 0; i < fwdReads.size(); ++i)
     {
         // generate fastq format of reads
-        ofsReads << '@' << i << std::endl;
+        ofsReads << '@' << i << "_" << offsets[i] << std::endl;
         ofsReads << fwdReads[i] << std::endl;
         ofsReads << '+' << i << std::endl;
         ofsReads << 'N' << std::endl;
     }
+    ofsReads.close();
+    ofsReads.open("genReadsRev_1err.fastq");
     for (size_t i = 0; i < revReads.size(); ++i)
     {
         const size_t id = i + fwdReads.size();
