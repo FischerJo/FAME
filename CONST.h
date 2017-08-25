1f6ae4012cea109c46ce5276ada7c6e3119ba975
Jonas Fischer <fischer@mmci.uni-saarland.de>
2017-08-25 14:24:19 +0200

    Added new recommendation for k

--- CONST.h
+++ CONST.h
@@ -11,27 +11,15 @@ namespace MyConst {
 //  -------- SET THESE VARIABLES --------
 //
 //
-// number of chromosomes in organism
-constexpr unsigned int CHROMNUM = 24;
-
-// (more than) size in bp of biggest chromosome in organism
-constexpr unsigned int CHROMMAX = 1000000000;
-
-// (more than) number of CpGs in organism
-constexpr unsigned int CPGMAX = 100000000;
 
 // maximum read length of the reads in bp
 constexpr unsigned int READLEN = 110;
 
-// number of reads that should be read per batch
-// (note that very large values may increase the required amount of RAM drastically)
-constexpr unsigned int CHUNKSIZE = 100000;
-
 // maximum number of times a k-mer is allowed to occur in the whole genome
-constexpr uint64_t KMERCUTOFF = 1000;
+constexpr uint64_t KMERCUTOFF = 500;
 
 // Number of cores that this program is allowed to occupy at any given point
-#define CORENUM 1
+#define CORENUM 30
 
 
 //  --------------------------------------
@@ -43,10 +31,23 @@ constexpr uint64_t KMERCUTOFF = 1000;
 // ------INTERNAL------
 // --------------------
 
+// number of chromosomes in organism
+constexpr unsigned int CHROMNUM = 24;
+
+// (more than) size in bp of biggest chromosome in organism
+constexpr unsigned int CHROMMAX = 1000000000;
+
+// (more than) number of CpGs in organism
+constexpr unsigned int CPGMAX = 100000000;
+
+// number of reads that should be read per batch
+// (note that very large values may increase the required amount of RAM drastically)
+// recommended is 100 000
+constexpr unsigned int CHUNKSIZE = 100000;
 
 // Length of a kmer in bp
-// recommended is 30
-constexpr unsigned int KMERLEN = 30;
+// recommended is 25
+constexpr unsigned int KMERLEN = 25;
 
 // Kmer bitmask to build reverse complement of 64bit kmer
 constexpr uint64_t KMERMASK = (KMERLEN == 32 ? 0xffffffffffffffffULL : (1ULL << 2*KMERLEN) - 1);
@@ -61,13 +62,15 @@ constexpr uint64_t HTABSIZE = 1ULL << 30;
 
 
 // window length for meta CpGs
-constexpr unsigned int WINLEN = 1 << 10;
+// recommended is 1024
+constexpr unsigned int WINLEN = 2048;
 
 // number of mismatches we allow
-constexpr unsigned int MISCOUNT = 1;
+// recommended is 2
+constexpr unsigned int MISCOUNT = 2;
 
 // threshold over which reads are immediately discarded if that many metaCpGs are hit
-constexpr unsigned int QUERYTHRESHOLD = 30000;
+// constexpr unsigned int QUERYTHRESHOLD = 30000;
 
 // Checks the usefulness of the set parameters
 void sanityChecks();
