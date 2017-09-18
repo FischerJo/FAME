3645903d6aa4fa1cdef31eca7d8dab32d352c8e8
Jonas Fischer <fischer@mmci.uni-saarland.de>
2017-09-18 11:43:33 +0200

    Updated plotting functions for RMSE related info

--- results/plot_it_bismark.R
+++ results/plot_it_bismark.R
@@ -17,20 +17,18 @@ data_real <- transform(data_real, RealRate = Methylated / (Unmethylated + Methyl
 data_real$RealRate <- replace(data_real$RealRate, is.na(data_real$RealRate), mean(data_real$RealRate, na.rm=T))
 
 
-data_my <- read.table(args[2],sep="\t",nrows=29401360,colClasses=c("integer","integer","integer","integer","integer","integer"))[,c(-5,-6)]
-colnames(data_my) <- c("Chrom","Pos","Meth","Unmeth")
-data_my <- transform(data_my, PredRate = Meth / (Unmeth + Meth))
+#
+data_bis <- read.table(args[2],nrows=29401360,colClasses=c("character","integer","character","integer","integer","character","character"))[,c(-3,-6,-7)]
+colnames(data_bis) <- c("Chrom","Pos","Meth","Unmeth")
+data_bis <- transform(data_bis, PredRate = Meth / (Unmeth + Meth))
 
-print(sum(data_my$Unmeth + data_my$Meth))
+mean_meth <- mean(data_bis$PredRate, na.rm=T)
 
-mean_meth <- mean(data_my$PredRate, na.rm=T)
-
-
-rmse_my <- round(sqrt(sum((data_real$Methylated - data_my$Meth)^2)/nrow(data_real)),digits=4)
+print(sum(data_bis$Unmeth + data_bis$Meth))
+rmse_bis <- round(sqrt(sum((data_real$Methylated - data_bis$Meth)^2)/nrow(data_real)),digits=4)
 pdf(args[3])
 # plot the correlation between aligned reads
-gg <- ggplot(cbind(data_real,data_my),aes(x=Methylated, y=Meth))
-#gg <- gg + geom_hex(bins = 100) + scale_fill_gradientn("", colours = rev(rainbow(10, end = 4/6)))
+gg <- ggplot(cbind(data_real,data_bis),aes(x=Methylated, y=Meth))
 gg <- gg + geom_hex(bins = 100) + scale_fill_gradientn("", colours = c("gray88","lightgoldenrod1","orange2","firebrick","black"), values=c(0,0.01,0.1,0.5,1))
 gg <- gg + theme_tufte()
 gg <- gg + theme(plot.margin = unit(c(2,2,2,2),"cm"))
@@ -41,14 +39,12 @@ gg <- gg + theme(plot.background = element_blank(),
                               plot.title = element_text(margin=margin(0,0,40,0))
                               )
 gg <- gg + labs(x="Number of generated methylated reads for this Position", y="Aligned methylated reads")
-gg <- gg + labs(title="Aligned methylated reads (METAL) vs\n actual number of methylated reads",subtitle=args[4])
+gg <- gg + labs(title="Aligned methylated reads (Bismark) vs\n actual number of methylated reads",subtitle=args[4])
 
-gg <- gg +  annotate("text", color="black", size=3, x = 10, y = max(data_my$Meth)-20, label = paste("RMSE:", rmse_my, sep=" "))
-
+gg <- gg +  annotate("text", color="black", size=3, x = 10, y = max(data_bis$Meth)-3, label = paste("RMSE:", rmse_bis, sep=" "))
 gg
-rmse_my <- round(sqrt(sum((data_real$Unmethylated - data_my$Unmeth)^2)/nrow(data_real)),digits=4)
-
-gg <- ggplot(cbind(data_real,data_my),aes(x=Unmethylated, y=Unmeth))
+rmse_bis <- round(sqrt(sum((data_real$Unmethylated - data_bis$Unmeth)^2)/nrow(data_real)),digits=4)
+gg <- ggplot(cbind(data_real,data_bis),aes(x=Unmethylated, y=Unmeth))
 gg <- gg + geom_hex(bins = 100) + scale_fill_gradientn("", colours = c("gray88","lightgoldenrod1","orange2","firebrick","black"), values=c(0,0.01,0.1,0.5,1))
 gg <- gg + theme_tufte()
 gg <- gg + theme(plot.margin = unit(c(2,2,2,2),"cm"))
@@ -59,25 +55,25 @@ gg <- gg + theme(plot.background = element_blank(),
                               plot.title = element_text(margin=margin(0,0,40,0))
                               )
 gg <- gg + labs(x="Number of generated unmethylated reads for this Position", y="Aligned unmethylated reads")
-gg <- gg + labs(title="Aligned unmethylated reads (METAL) vs\n actual number of unmethylated reads",subtitle=args[4])
-gg <- gg +  annotate("text", color="black", size=3, x = 10, y = max(data_my$Unmeth)-60, label = paste("RMSE:", rmse_my, sep=" "))
+gg <- gg + labs(title="Aligned unmethylated reads (Bismark) vs\n actual number of unmethylated reads",subtitle=args[4])
+
+gg <- gg +  annotate("text", color="black", size=3, x = 10, y = max(data_bis$Unmeth)-10, label = paste("RMSE:", rmse_bis, sep=" "))
 gg
 
-na_perc <- round(sum(is.na(data_my$PredRate))/nrow(data_my),digits=2)
+na_perc <- round(sum(is.na(data_bis$PredRate))/nrow(data_bis),digits=2)
 
-data_my$PredRate <- replace(data_my$PredRate, is.na(data_my$PredRate), mean_meth)
+data_bis$PredRate <- replace(data_bis$PredRate, is.na(data_bis$PredRate), mean_meth)
 
 
-
-cor_pe_my <- round(cor(data_real[,5], data_my[,5], method="pearson",use='complete.obs'), digits=2)
-cor_sp_my <- round(cor(data_real[,5], data_my[,5], method="spearman",use='complete.obs'), digits=2)
-rmse_my <- round(sqrt(sum((data_real$RealRate - data_my$PredRate)^2)/nrow(data_real)),digits=4)
+cor_pe_bis <- round(cor(data_real[,5], data_bis[,5], method="pearson",use='complete.obs'), digits=2)
+cor_sp_bis <- round(cor(data_real[,5], data_bis[,5], method="spearman",use='complete.obs'), digits=2)
+rmse_bis <- round(sqrt(sum((data_real$RealRate - data_bis$PredRate)^2)/nrow(data_real)),digits=4)
 
 #sample_ids <- sample(nrow(data_real),20000)
-#round(dcor(data_real[sample_ids,5], data_my[sample_ids,5]),digits=2)
+#round(dcor(data_real[sample_ids,5], data_bis[sample_ids,5]),digits=2)
 
-
-gg <- ggplot(cbind(data_real,data_my),aes(x=PredRate, y=RealRate))
+# plot bismark data
+gg <- ggplot(cbind(data_real,data_bis),aes(x=PredRate, y=RealRate))
 #    geom_point(alpha=0.2,size=0.3)
 gg <- gg + geom_hex(bins = 100) + scale_fill_gradientn("", colours = c("gray88","lightgoldenrod1","orange2","firebrick","black"), values=c(0,0.01,0.1,0.5,1))
 gg <- gg + theme_tufte()
@@ -90,14 +86,14 @@ gg <- gg + theme(plot.background = element_blank(),
                               )
 gg <- gg + labs(x="Predicted methylation rate", y="Real methylation rate")
 
-gg <- gg +  annotate("text", color="darkred", size=3, x = 0.75, y = 0.15, label = paste("Spearmen:", cor_sp_my, sep=" "))
+gg <- gg +  annotate("text", color="darkred", size=3, x = 0.75, y = 0.15, label = paste("Spearmen:", cor_sp_bis, sep=" "))
 
-gg <- gg +  annotate("text", color="darkred", size=3, x = 0.75, y = 0.1, label = paste("Pearson:", cor_pe_my, sep=" "))
-gg <- gg +  annotate("text", color="darkred", size=3, x = 0.75, y = 0.2, label = paste("RMSE:", rmse_my, sep=" "))
+gg <- gg +  annotate("text", color="darkred", size=3, x = 0.75, y = 0.1, label = paste("Pearson:", cor_pe_bis, sep=" "))
+gg <- gg +  annotate("text", color="darkred", size=3, x = 0.75, y = 0.2, label = paste("RMSE:", rmse_bis, sep=" "))
 #gg <- gg +  annotate("text", color="darkred", size=3, x = 0.75, y = 0.9, label = paste("Proportion of NAs:",na_perc, sep=" "))
 
 gg <- gg + annotate("rect", xmin = 0.6, xmax = 0.9, ymin = 0.08, ymax = 0.22, alpha = .1)
-gg <- gg + labs(title="Methylation rates of METAL",subtitle=args[4])
+gg <- gg + labs(title="Methylation rates of Bismark",subtitle=args[4])
 
 gg
 
--- results/plot_it_bsmap.R
+++ results/plot_it_bsmap.R
@@ -1,6 +1,7 @@
 require(ggplot2)
 require(reshape2)
 require(ggthemes)
+require(energy)
 
 # Arguments:
 # 1)  file with real values (how they were generated)
@@ -10,21 +11,31 @@ require(ggthemes)
 
 args <- commandArgs(trailingOnly = TRUE)
 
-data_real <- read.table(args[1],header=TRUE,sep="\t")[,-5]
+data_real <- read.table(args[1],header=F,sep="\t",nrows=29401360,colClasses=c("integer","integer","integer","integer","double"))[,-5]
+colnames(data_real) <- c("Chromosome","Position","Unmethylated","Methylated")
 data_real <- transform(data_real, RealRate = Methylated / (Unmethylated + Methylated))
+data_real$RealRate <- replace(data_real$RealRate, is.na(data_real$RealRate), mean(data_real$RealRate, na.rm=T))
 
-mean_meth <- mean(data_real$RealRate)
 
 #
-data_bis <- read.table(args[2])[,c(-3,-6,-7)]
-colnames(data_bis) <- c("Chrom","Pos","Unmeth","Meth")
-data_bis <- transform(data_bis, PredRate = Meth / (Unmeth + Meth))
+data_bsm <- read.table(args[2])[,c(2,5,7,8,9,10)]
+data_bsm[,3] <- data_bsm[,3] + data_bsm[,5]
+data_bsm[,4] <- data_bsm[,4] + data_bsm[,6]
+data_bsm[,1] <- data_bsm[,1] + 1
+data_bsm <- data_bsm[,c(-5,-6)]
+colnames(data_bsm) <- c("Position","PredRate","Unmeth","Meth")
+data_bsm$PredRate <- data_bsm$Meth / (data_bsm$Unmeth + data_bsm$Meth)
 
-print(sum(data_bis$Unmeth + data_bis$Meth))
+data <- merge(data_real, data_bsm, by=c("Position"))
 
+
+#mean_meth <- mean(data_bsm$PredRate, na.rm=T)
+
+#print(sum(data_bsm$Unmeth + data_bsm$Meth))
+rmse_bis <- round(sqrt(sum((data$Methylated - data$Meth)^2)/nrow(data)),digits=4)
 pdf(args[3])
 # plot the correlation between aligned reads
-gg <- ggplot(cbind(data_real,data_bis),aes(x=Methylated, y=Meth))
+gg <- ggplot(data,aes(x=Methylated, y=Meth))
 gg <- gg + geom_hex(bins = 100) + scale_fill_gradientn("", colours = rev(rainbow(10, end = 4/6)))
 gg <- gg + theme_tufte()
 gg <- gg + theme(plot.margin = unit(c(2,2,2,2),"cm"))
@@ -37,8 +48,10 @@ gg <- gg + theme(plot.background = element_blank(),
 gg <- gg + labs(x="Number of generated methylated reads for this Position", y="Aligned methylated reads")
 gg <- gg + labs(title="Aligned methylated reads (Bismark) vs\n actual number of methylated reads",subtitle=args[4])
 
+gg <- gg +  annotate("text", color="black", size=3, x = 10, y = max(data$Meth)-3, label = paste("RMSE:", rmse_bis, sep=" "))
 gg
-gg <- ggplot(cbind(data_real,data_bis),aes(x=Unmethylated, y=Unmeth))
+rmse_bis <- round(sqrt(sum((data$Unmethylated - data$Unmeth)^2)/nrow(data)),digits=4)
+gg <- ggplot(data,aes(x=Unmethylated, y=Unmeth))
 gg <- gg + geom_hex(bins = 100) + scale_fill_gradientn("", colours = rev(rainbow(10, end = 4/6)))
 gg <- gg + theme_tufte()
 gg <- gg + theme(plot.margin = unit(c(2,2,2,2),"cm"))
@@ -51,19 +64,20 @@ gg <- gg + theme(plot.background = element_blank(),
 gg <- gg + labs(x="Number of generated unmethylated reads for this Position", y="Aligned unmethylated reads")
 gg <- gg + labs(title="Aligned unmethylated reads (Bismark) vs\n actual number of unmethylated reads",subtitle=args[4])
 
+gg <- gg +  annotate("text", color="black", size=3, x = 10, y = max(data$Unmeth)-10, label = paste("RMSE:", rmse_bis, sep=" "))
 gg
 
-na_perc <- round(sum(is.na(data_bis$PredRate))/nrow(data_bis),digits=2)
-
-data_bis$PredRate <- replace(data_bis$PredRate, is.na(data_bis$PredRate), mean_meth)
 
 
-cor_pe_bis <- round(cor(data_real[,5], data_bis[,5], method="pearson",use='complete.obs'), digits=2)
-cor_sp_bis <- round(cor(data_real[,5], data_bis[,5], method="spearman",use='complete.obs'), digits=2)
-rmse_bis <- round(sqrt(sum((data_real$RealRate - data_bis$PredRate)^2)/nrow(data_real)),digits=4)
+cor_pe_bis <- round(cor(data$RealRate, data$PredRate, method="pearson",use='complete.obs'), digits=2)
+cor_sp_bis <- round(cor(data$RealRate, data$PredRate, method="spearman",use='complete.obs'), digits=2)
+rmse_bis <- round(sqrt(sum((data$RealRate - data$PredRate)^2)/nrow(data)),digits=4)
+
+#sample_ids <- sample(nrow(data_real),20000)
+#round(dcor(data_real[sample_ids,5], data_bsm[sample_ids,5]),digits=2)
 
 # plot bismark data
-gg <- ggplot(cbind(data_real,data_bis),aes(x=PredRate, y=RealRate))
+gg <- ggplot(data,aes(x=PredRate, y=RealRate))
 #    geom_point(alpha=0.2,size=0.3)
 gg <- gg + geom_hex(bins = 200) + scale_fill_gradientn("", colours = rev(rainbow(10, end = 4/6)))
 gg <- gg + theme_tufte()
@@ -75,14 +89,14 @@ gg <- gg + theme(plot.background = element_blank(),
                               plot.title = element_text(margin=margin(0,0,40,0))
                               )
 gg <- gg + labs(x="Predicted methylation rate", y="Real methylation rate")
+gg <- gg + annotate("rect", xmin = 0.6, xmax = 0.9, ymin = 0.08, ymax = 0.22, alpha = .6)
+gg <- gg +  annotate("text", color="white", size=3, x = 0.75, y = 0.15, label = paste("Spearmen:", cor_sp_bis, sep=" "))
 
-gg <- gg +  annotate("text", color="darkred", size=3, x = 0.75, y = 0.15, label = paste("Spearmen:", cor_sp_bis, sep=" "))
+gg <- gg +  annotate("text", color="white", size=3, x = 0.75, y = 0.1, label = paste("Pearson:", cor_pe_bis, sep=" "))
+gg <- gg +  annotate("text", color="white", size=3, x = 0.75, y = 0.2, label = paste("RMSE:", rmse_bis, sep=" "))
 
-gg <- gg +  annotate("text", color="darkred", size=3, x = 0.75, y = 0.1, label = paste("Pearson:", cor_pe_bis, sep=" "))
-gg <- gg +  annotate("text", color="darkred", size=3, x = 0.75, y = 0.2, label = paste("RMSE:", rmse_bis, sep=" "))
-#gg <- gg +  annotate("text", color="darkred", size=3, x = 0.75, y = 0.9, label = paste("Proportion of NAs:",na_perc, sep=" "))
 
-gg <- gg + annotate("rect", xmin = 0.6, xmax = 0.9, ymin = 0.08, ymax = 0.22, alpha = .1)
+
 gg <- gg + labs(title="Methylation rates of Bismark",subtitle=args[4])
 
 gg
